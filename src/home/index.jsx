4e280605284b61d6a9b21ff795d2b4e45e95f8ea
Aditya Paliwal <adityapaliwal243@gmail.com>
2024-08-24 19:01:22 +0530

    create header with google ,github,microsoft authentication (clerck) with landing page

---
+++ src/home/index.jsx
@@ -0,0 +1,14 @@
+import Header from "@/components/customs/Header";
+import { UserButton } from "@clerk/clerk-react";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      Landing Page
+    </div>
+  );
+};
+
+export default Home;
